696a3da3177eb117157925f073195ef51024bc16
Charles Bergeron <chuck.bergeron@gmail.com>
2023-06-15 10:06:26 -0700

    cleanup

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -1,7 +1,6 @@
 export { createOutputPath } from './createOutputPath'
 export { updateStatusFailure, updateStatusSuccess, createStatus } from './status'
 export { createExitCode } from './createExitCode'
-export { makeGraphQlQuery } from './makeGraphQlQuery'
 export { getProvider } from './getProvider'
 export { writeToOutput, writeStatus } from './writeOutput'
 export * from './prizeAmounts'
