309974d1b5ef34de464e3d8c69553239b1f89596
Charles Bergeron <chuck.bergeron@gmail.com>
2023-08-23 10:42:06 -0700

    update for optimism goerli and eth goerli

--- src/lib/utils/getProvider.ts
+++ src/lib/utils/getProvider.ts
@@ -7,14 +7,10 @@ type Providers = { [k: string]: JsonRpcProvider };
 const providers: Providers = {
   // mainnets
   1: new JsonRpcProvider(process.env.ETHEREUM_MAINNET_RPC_URL),
-  137: new JsonRpcProvider(process.env.POLYGON_MAINNET_RPC_URL),
   10: new JsonRpcProvider(process.env.OPTIMISM_MAINNET_RPC_URL),
   // testnets
   5: new JsonRpcProvider(process.env.ETHEREUM_GOERLI_RPC_URL),
-  11155111: new JsonRpcProvider(process.env.ETHEREUM_SEPOLIA_RPC_URL),
-  80001: new JsonRpcProvider(process.env.POLYGON_MUMBAI_RPC_URL),
   420: new JsonRpcProvider(process.env.OPTIMISM_GOERLI_RPC_URL),
-  421613: new JsonRpcProvider(process.env.ARBITRUM_GOERLI_RPC_URL),
 }
 
 export const getProvider = (chainId: string): JsonRpcProvider => {
